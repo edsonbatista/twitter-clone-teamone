167c7767485fef31490c1e7f9bae27efea252da8
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-10-02 22:36:40 -0300

    redirect user after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :birthdate])
   end
+
+  def after_sign_in_path_for(resource)
+    '/users'# redirecionar para a timeline com os posts quando pronta
+  end
+
+  # def after_sign_up_path_for(resource)
+  #   redirecionar para user profile edit       
+  # end
 end
