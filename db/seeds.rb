3b20e16c4470529792a9e2ca96c02bb1fd16ded0
rafaelBadan <rafael.badan@gmail.com>
2017-10-04 20:53:20 -0300

    implemented first part of a timeline (lacks some controllers implementation)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+leo = User.where(email: "leonardo_scorza@gmail.com").first_or_create! do |user|
+        user.name                    = "Leonardo Scorza"
+        user.username                = "@leo_scorza"
+        user.birthdate               = "18/04/1887"
+        user.city                    = "São Paulo"
+        user.password                = "123456"
+        user.password_confirmation   = "123456"
+      end
+
+jho = User.where(email: "jhonatanbmartins@gmail.com").first_or_create! do |user|
+        user.name                    = "Jhonatan Martins"
+        user.username                = "@jhow_martins"
+        user.birthdate               = "18/04/1998"
+        user.city                    = "Santa Bárbara d' Oeste"
+        user.password                = "123456"
+        user.password_confirmation   = "123456"
+      end
+
+rafa = User.where(email: "rafael_badan@gmail.com").first_or_create! do |user|
+         user.name                    = "Rafael Badan"
+         user.username                = "@rafael_badan"
+         user.birthdate               = "18/04/1981"
+         user.city                    = "São Paulo"
+         user.password                = "123456"
+         user.password_confirmation   = "123456"
+      end
+
+Post.where(content: "Bom dia!! Nova postagem no #OneBitCode").first_or_create! do |post|
+  post.user = leo
+end
+
+Post.where(content: "Quinta tem OneBitTalk com Jackson Pires <3 #onebittalk").first_or_create! do |post|
+  post.user = leo
+end
+
+Post.where(content: "Nova turma do bootcamp no próximo mês, bora ser um Super Full Stack!!").first_or_create! do |post|
+  post.user = leo
+end
+
+Post.where(content: "Partiu jogar um lolzinho #tipomid").first_or_create! do |post|
+  post.user = jho
+end
+
+Post.where(content: "#Partiu facul!! #fui").first_or_create! do |post|
+  post.user = leo
+end
+
+Post.where(content: "Chegando do trabalho #cansado").first_or_create! do |post|
+  post.user = rafa
+end
+
+Post.where(content: "Vários freela pra fazer, bora madrugar #ruby #rails").first_or_create! do |post|
+  post.user = rafa
+end
+
+jho.active_relationships.create(followed_id: 1)
+jho.active_relationships.create(followed_id: 3)
+
+leo.active_relationships.create(followed_id: 2)
+leo.active_relationships.create(followed_id: 3)
+
+rafa.active_relationships.create(followed_id: 1)
+rafa.active_relationships.create(followed_id: 2)
+
+# Relationship.where(follower: jho, following: leo).first_or_create!
 #
-# Examples:
+# Relationship.where(follower: rafa, following: leo).first_or_create!
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Relationship.where(follower: leo, following: rafa).first_or_create!
+#
+# Relationship.where(follower: jho, following: rafa).first_or_create!
+#
+# Relationship.where(follower: leo, following: jho).first_or_create!
+#
+# Relationship.where(follower: rafa, following: jho).first_or_create!
